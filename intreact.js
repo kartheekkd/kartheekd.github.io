fbea7bf2413c7d1c1b8b07984dbf7e9a68f33468
kartheekkd <125897226+kartheekkd@users.noreply.github.com>
2023-02-21 19:25:04 +0530

    Update intreact.js

--- intreact.js
+++ intreact.js
@@ -4,6 +4,6 @@ const message = document.getElementById("message");
 img.addEventListener("click", revealGift);
 
 function revealGift() {
-  img.src = "[Insert image link or file location for the surprise gift]";
+  img.src = "./komugi.jpg";
   message.textContent = "Surprise! Happy Birthday! Love you!";
 }
