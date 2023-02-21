6d64ce7673f18b236bcfc53c682991dd239e2541
kartheekkd <125897226+kartheekkd@users.noreply.github.com>
2023-02-21 19:31:25 +0530

    Delete intreact.js

--- intreact.js
+++
@@ -1,9 +0,0 @@
-const img = document.getElementById("birthday-img");
-const message = document.getElementById("message");
-
-img.addEventListener("click", revealGift);
-
-function revealGift() {
-  img.src = "./komugi.jpg";
-  message.textContent = "Surprise! Happy Birthday! Love you!";
-}
